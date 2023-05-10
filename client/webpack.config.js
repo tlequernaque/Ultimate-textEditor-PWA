ea370a132ef9db4c52ef10e24b45662eb82542df
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-05-10 03:23:01 -0400

    finished setting webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -18,36 +16,36 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      // new HtmlWebpackPlugin({
-      //   template: './index.html',
-      //   title: 'Contact Cards'
-      // }),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
      
-      // // Injects our custom service worker
-      // new InjectManifest({
-      //   swSrc: './src-sw.js',
-      //   swDest: 'src-sw.js',
-      // }),
+      // Injects our custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
 
-      // // Creates a manifest.json file.
-      // new WebpackPwaManifest({
-      //   fingerprints: false,
-      //   inject: true,
-      //   name: 'Contact Cards',
-      //   short_name: 'Contact',
-      //   description: 'Never forget your contacts!',
-      //   background_color: '#225ca3',
-      //   theme_color: '#225ca3',
-      //   start_url: './',
-      //   publicPath: './',
-      //   icons: [
-      //     {
-      //       src: path.resolve('src/images/logo.png'),
-      //       sizes: [96, 128, 192, 256, 384, 512],
-      //       destination: path.join('assets', 'icons'),
-      //     },
-      //   ],
-      // }),
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Text Editor with offline capabilities using IndexedDB',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
       
     ],
 
