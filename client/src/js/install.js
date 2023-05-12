fa8c33be3a406d1e11d0f85d83430d379e5f7d3c
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-05-11 21:51:28 -0400

    deleted some comments

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -9,7 +9,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
     butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
 
@@ -26,7 +26,7 @@ butInstall.addEventListener('click', async () => {
     butInstall.classList.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
+
 window.addEventListener('appinstalled', (event) => {
     // Clear prompt
     window.deferredPrompt = null;
