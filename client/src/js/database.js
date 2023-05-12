e1182e1e703486984faf63cfd9832a08d7e33c6d
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-05-11 21:46:40 -0400

    fixed some code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,22 +14,21 @@ const initdb = async () =>
 
 
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
+  // console.error('putDb not implemented');
   const jateDB = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
+  const tx = jateDB.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, todo: content });
+  const request = store.put({ jate: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
+  // console.error('getDb not implemented');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.get(id);
+  const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
   return result;
